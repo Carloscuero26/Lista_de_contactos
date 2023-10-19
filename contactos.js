f466e91c06de0852d67b092c233b27182ac0c2e0
Carlos Cuero <pipecuerosuarez@gmail.com>
2023-10-19 11:50:09 -0500

    Lista de cotacto actualizada

--- contactos.js
+++ contactos.js
@@ -1,22 +1,53 @@
 let listaContacto = [
-    "Carlos Diaz",
-    "Jose Escorcia",
-    "Maria Mercado",
-    "Carlos Catillo",
+    {
+      id: 1,
+      nombre: "carlos",
+      apellido: "Cuero",
+      telefono: 3244610057,
+      ubicacion: "Colombia",
+      ciudad: "Cartagena- Bolivar",
+      direccion: "Calla 20 #59b-75",
+    },
+    {
+      id: 2,
+      nombre: "Maria",
+      apellido: "Mercado",
+      telefono: 3002469432,
+      ubicacion: "Colombia",
+      ciudad: "Sincelejo",
+      direccion: "Tranversal 19 #12c-91",
+    },
+    {
+      id: 3,
+      nombre: "Jose",
+      apellido: "Escorcia",
+      telefono: 3204578930,
+      ubicacion: "Colombia",
+      ciudad: "Barranquilla",
+      direccion: "Diagonal 5 #47aa-134",
+    },
   ];
-  
-  function agregarContacto(nuevoContacto) {
-    listaContacto.push(nuevoContacto);
+  function añadirContacto(contacto) {
+    listaContacto.push(contacto);
   }
   
-  agregarContacto("Maria Mercado");
+  let nuevoContacto = {
+    id: 4,
+    nombre: "Kevin",
+    apellido: "Ortega",
+    telefono: 3201788746,
+    ubicacion: "Colombia",
+    ciudad: "Bogota",
+    direccion: "Carrera 21 #72-87",
+  };
   
-  function borrarContacto(borrar) {
-    listaContacto.splice(borrar, 1);
-  }
+  añadirContacto(nuevoContacto);
   
-  function imprimirContacto() {
-    for (let i = 0; i < listaContacto.length; i++) {
-      console.log(listaContacto[i]);
+  function chaoContacto(chao) {
+    const posicion = listaContacto.indexOf(chao);
+    if (posicion !== -1) {
+      listaContacto.splice(posicion, 1);
     }
   }
+  chaoContacto(listaContacto[1]);
+  console.log(listaContacto);
